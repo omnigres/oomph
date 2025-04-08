865302052050b67eec3fe56ba92a536f8f8a8e0a
Yurii Rashkovskii <yrashk@gmail.com>
2025-04-08 12:46:37 -0700

    Problem: solving common problems in C++ code
    I keep shuttling snippets solving some more-or-less generic problems.
    
    Solution: extract a headers-only library (oomph)
    
    Maybe it is Omnigres' Out-of-the-box Meta-Programming Helper? Something like this.

---
+++ tests/overload.cpp
@@ -0,0 +1,12 @@
+#include "doctest.h"
+
+#include <oomph/overload.hpp>
+
+TEST_CASE("overload dispatch") {
+  auto overloaded = oomph::overload {
+    [](int) { return 0; },
+    [](float) { return 1; },
+  };
+  REQUIRE(overloaded(1) == 0);
+  REQUIRE(overloaded(1.1f) == 1);
+}
